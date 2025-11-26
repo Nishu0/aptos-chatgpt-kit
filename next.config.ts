ad63ff86daaed687d2837a7d2be461d6f2dc98c8
Nishu0 <itsthakkarnisarg@gmail.com>
2025-11-26 15:17:27 +0530

    build patch

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
 import type { NextConfig } from "next";
 import { baseURL } from "./baseUrl";
+import path from "path";
+
+const stubPath = path.resolve('./lib/keyv-stub.js');
 
 const nextConfig: NextConfig = {
   assetPrefix: baseURL,
+  turbopack: {
+    resolveAlias: {
+      // Alias optional keyv adapters to stub module
+      '@keyv/redis': stubPath,
+      '@keyv/mongo': stubPath,
+      '@keyv/sqlite': stubPath,
+      '@keyv/postgres': stubPath,
+      '@keyv/mysql': stubPath,
+      '@keyv/etcd': stubPath,
+      '@keyv/offline': stubPath,
+      '@keyv/tiered': stubPath,
+    },
+  },
 };
 
 export default nextConfig;
